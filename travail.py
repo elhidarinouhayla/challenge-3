2510decefbcb437a0b569f1beb4063bb279422e6
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-09-25 15:34:55 +0100

    update

--- travail.py
+++ travail.py
@@ -14,7 +14,27 @@ for produits,prix  in A:
 A.sort(key=lambda x: x[1])
 print(A)
 
+def order(x):
+    return x[1] 
+A.sort()
+print(A)
+
+
 tup = tuple(A)
 print(tup)
+
+ma = max(tup,key=lambda a: a[1])
+print(f"{ma} est le poduit le plus cher")
+mi = min(tup,key=lambda b: b[1])
+print(f"{mi} est le produit le moins cher")
+
+def bonus(prix):
+    return produits + LUXE
+    for x in prix:
+        if prix > 1000:
+            resultat = list(map(bonus,prix))
+            print(resultat)
+
+
 
 
